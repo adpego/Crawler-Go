8c2fb972f60b573c2b2491fb80101a38e925a492
adpego <adpego7@gmail.com>
2021-07-19 04:44:22 +0200

    Adding some flags to CLI

--- crawler.go
+++ crawler.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/gocolly/colly"
 	"sort"
 	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+var (
+	word              string
+	maxDepth          int
+	verbose           bool
+	noOutput          bool
+	allAllowedDomains bool
+	allowedDomains    string
+	filename          string
 )
 
 func find(slice []string, elemFind string) bool {
@@ -16,26 +28,48 @@ func find(slice []string, elemFind string) bool {
 	return false
 }
 
+func appendIfNotExist(slice []string, elem string) []string {
+	if !find(slice, elem) {
+		slice = append(slice, elem)
+	}
+	return slice
+}
+
+func init() {
+	flag.StringVar(&word, "u", "", "Url to start the crawler")
+	flag.IntVar(&maxDepth, "d", 3, "Max depth for crawling")
+	flag.BoolVar(&verbose, "v", false, "Verbose option, show links visiting")
+	flag.BoolVar(&noOutput, "no", false, "No output - option, for don't show the result to command line, requirement -f parameter")
+	flag.StringVar(&filename, "f", "", "File to save the output")
+	flag.StringVar(&allowedDomains, "ad", "", "Allowed domains to crawl. (Default value are the url)")
+	flag.BoolVar(&allAllowedDomains, "all", false, "Allow all domains to crawl, not recomended for a uncontrolled environment")
+
+}
+
 func main() {
+
+	flag.Parse()
 
 	var urls []string
 	var images []string
 	var scripts []string
 	var links []string
 
-	c := colly.NewCollector(
+	/*c := colly.NewCollector(
 		colly.MaxDepth(1),
 		colly.AllowedDomains("adpego.com"),
-	)
+	)*/
+	c := colly.NewCollector()
+	c.MaxDepth = 1
+	colly.AllowedDomains("adpego.com")
 
 	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		if !strings.HasPrefix(e.Attr("href"), "#") {
 			url := e.Request.AbsoluteURL(e.Attr("href"))
-			found := find(urls, url)
-			if !found {
-				urls = append(urls, url)
-			}
+
+			urls = appendIfNotExist(urls, url)
+
 			fmt.Println("VISITING", e.Attr("href"))
 			e.Request.Visit(e.Attr("href"))
 		}
@@ -44,29 +78,23 @@ func main() {
 
 	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
 		url := e.Request.AbsoluteURL(e.Attr("src"))
-		found := find(images, url)
-		if !string.HasPrefix(e.Attr("src"), "data:image") {
-			if !found {
-				images = append(images, url)
-			}
-		]
+
+		if !strings.HasPrefix(e.Attr("src"), "data:image") {
+			images = appendIfNotExist(images, url)
+		}
 
 	})
 
 	c.OnHTML("link[href]", func(e *colly.HTMLElement) {
 		url := e.Request.AbsoluteURL(e.Attr("src"))
-		found := find(links, url)
-		if !found {
-			links = append(links, url)
-		}
+		links = appendIfNotExist(links, url)
+
 	})
 
 	c.OnHTML("script[src]", func(e *colly.HTMLElement) {
 		url := e.Request.AbsoluteURL(e.Attr("src"))
-		found := find(scripts, url)
-		if !found {
-			scripts = append(scripts, url)
-		}
+		scripts = appendIfNotExist(scripts, url)
+
 	})
 
 	c.Visit("https://adpego.com")
